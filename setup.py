3fe3b3aa52b0e059788f52eca1509d79d5d39f6f
Guido Stevens <guido.stevens@cosent.nl>
2012-10-09 13:22:08 +0200

    reindent for better pep8

--- setup.py
+++ setup.py
@@ -13,37 +13,38 @@ long_description = (
     open('CHANGES.txt').read()
     + '\n')
 
-setup(name='plonesocial.suite',
-      version=version,
-      description="Pre-integrated social business suite for Plone",
-      long_description=long_description,
-      # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
+setup(
+    name='plonesocial.suite',
+    version=version,
+    description="Pre-integrated social business suite for Plone",
+    long_description=long_description,
+    # Get more strings from
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
         "Programming Language :: Python",
         ],
-      keywords='plone socbiz social microblogging',
-      author='Guido Stevens',
-      author_email='guido.stevens@cosent.net',
-      url='http://github.com/cosent/plonesocial.suite',
-      license='gpl',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['plonesocial'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'plonesocial.microblog',
-          'plonesocial.activitystream',
-          'plonesocial.network',
-          'loremipsum',
-          'plone.api',
-          # -*- Extra requirements: -*-
-      ],
-      extras_require={'test': ['plone.app.testing']},
-      entry_points="""
+    keywords='plone socbiz social microblogging',
+    author='Guido Stevens',
+    author_email='guido.stevens@cosent.net',
+    url='http://github.com/cosent/plonesocial.suite',
+    license='gpl',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['plonesocial'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'plonesocial.microblog',
+        'plonesocial.activitystream',
+        'plonesocial.network',
+        'loremipsum',
+        'plone.api',
+        # -*- Extra requirements: -*-
+        ],
+    extras_require={'test': ['plone.app.testing']},
+    entry_points="""
       # -*- Entry points: -*-
   	  [z3c.autoinclude.plugin]
   	  target = plone
       """,
-      )
+    )
